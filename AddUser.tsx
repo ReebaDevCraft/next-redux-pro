aaacfc06b209b332c52cea22c5cc732e1e664053
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-12 14:36:06 +0500

    Add files via upload

---
+++ AddUser.tsx
@@ -0,0 +1,14 @@
+export default function AddUser() {
+    return (
+      <div>
+        <h3>Add User</h3>
+        <input
+          type="text"
+          placeholder="Add New User"/>
+        <button>
+          Add User
+        </button>
+      </div>
+    );
+  }
+  
